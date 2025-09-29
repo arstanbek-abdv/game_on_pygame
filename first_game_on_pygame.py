3a0943415738ab4c7cd9443b8cabfe0d8d03b143
arstanbek-abd <abdyldaevarstanbek79@gmail.com>
2025-09-29 19:16:42 +0600

    Added Clock() for controlling the Frame Rate and gray background

--- first_game_on_pygame.py
+++ first_game_on_pygame.py
@@ -6,11 +6,10 @@ class AlienInvasion:
     def __init__(self):
         # Initialize the game, and create game resources
         pygame.init()
-        
         self.screen = pygame.display.set_mode((1200, 800))
-       
         pygame.display.set_caption('Alien Invasion')
-    
+        self.clock = pygame.time.Clock() # Clock ensures that the game will run across systems consistently by managing (slowing or speeding up FPS)
+        self.bg_color = (230,230,230)
     def run_game(self):
         # Start the main loop for the game
         while True:
@@ -18,11 +17,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-    
-    # Make the most recently drawn screen visble
-        pygame.display.flip()
-
-
+            pygame.display.flip() # Make the most recently drawn screen visble
+            self.clock.tick(60)
+            self.screen.fill(self.bg_color)
 # Instantiating a game class and running the game 
 if __name__ == '__main__':
     ai = AlienInvasion()
