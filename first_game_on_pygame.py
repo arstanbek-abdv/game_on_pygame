589fdc6481b2f62343cf2b97f3c86f036f5c3d68
arstanbek-abd <abdyldaevarstanbek79@gmail.com>
2025-09-29 17:57:28 +0600

    Making the screen visible

--- first_game_on_pygame.py
+++ first_game_on_pygame.py
@@ -1,3 +1,29 @@
 import sys
 import pygame 
 
+class AlienInvasion:
+    # Overall class to manage the game
+    def __init__(self):
+        # Initialize the game, and create game resources
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+       
+        pygame.display.set_caption('Alien Invasion')
+    
+    def run_game(self):
+        # Start the main loop for the game
+        while True:
+            # Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+    # Make the most recently drawn screen visble
+        pygame.display.flip()
+
+
+# Instantiating a game class and running the game 
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
